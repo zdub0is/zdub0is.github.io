72c2eb81f102a4bc582eee90f06b38588915105c
Zeke <az.duboiz@gmail.com>
2023-04-05 12:18:24 -0700

    works is working! (ftmp)

--- src/components/TarotCarousel.js
+++ src/components/TarotCarousel.js
@@ -1,23 +1,59 @@
-import React from 'react';
-import { Carousel } from 'react-responsive-carousel';
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // Styles for the carousel
-import '.././Tarot.css'; // Styles for your tarot cards
+import React from "react";
+import { Card } from "react-bootstrap";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { EffectCards, Mousewheel } from "swiper";
+import { useNavigate } from "react-router-dom";
+import "swiper/css";
+import "swiper/css/effect-cards";
+import ".././Tarot.css";
+import ffxivProfitThumb from "../assets/xivprofit-thumb.png";
+import zduboisThumb from "../assets/zdubois-thumb.png";
+import comingSoonThumb from "../assets/coming-soon-thumb.png";
 
 const TarotCarousel = () => {
+  const navigate = useNavigate();
+  const tarotData = [
+    { id: 1, text: "FFXIV Profit", path: "/works/ffxivprofit", img: ffxivProfitThumb },
+    { id: 2, text: "zdubois.com", path: "/works/zdubois", img: zduboisThumb },
+    { id: 3, text: "Coming Soon..." },
+  ];
+
+  const renderItem = (item) => {
     return (
-      <Carousel>
+      <div key={item.id} className="tarot-card-wrapper">
         <div className="tarot-card">
-          <div className="card-header">
-            <h2>Project 1</h2>
-          </div>
-          <div className="card-content">
-            <p>Project description</p>
+          <div className="tarot-card-inner">
+            <div className="tarot-card-face tarot-card-back"></div>
+            <Card
+              className="tarot-card-face tarot-card-front"
+              onClick={() => (item.path ? navigate(item.path) : null)}
+              style= {item.img ? {backgroundImage: `url(${item.img})`} : {backgroundImage: `url(${comingSoonThumb})`}}
+            >
+            </Card>
           </div>
         </div>
-        {/* Add more tarot cards as needed */}
-      </Carousel>
+      </div>
     );
   };
-  
-  export default TarotCarousel;
-  
+
+  return (
+    <div>
+      <Swiper
+        effect={"cards"}
+        grabCursor={true}
+        mousewheel={true}
+        simulateTouch={true}
+        modules={[EffectCards, Mousewheel]}
+        centeredSlides={true}
+        rebuildOnUpdate={true}
+      >
+        {tarotData.map((item) => (
+          <SwiperSlide key={item.id}>{renderItem(item)}</SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
+};
+
+
+export default TarotCarousel;
--- src/components/Works.js
+++ src/components/Works.js
@@ -1,12 +1,16 @@
-import CornerFrames from "./Corners";
-import thickCornerFrame from "../assets/thick-fancy-frame.png";
+// import CornerFrames from "./Corners";
+// import thickCornerFrame from "../assets/thick-fancy-frame.png";
 import TarotCarousel from './TarotCarousel';
+// import { Row } from "react-bootstrap";
 
 function Works() {
   return(
     <div>
-<CornerFrames imageUrl={thickCornerFrame} alt="Thick Fancy Frame" corners={[true, true, false, false]} />
+<div className="pirata sub-page-hero">Works</div>
+      {/*<div className="sub-page-hero-line rounded-pill" />
+      <div className="sub-page-hero-text pirata">Home</div>*/}
   <TarotCarousel />
+  
     </div>
   )
 }
